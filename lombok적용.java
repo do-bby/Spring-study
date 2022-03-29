67f6fab79f91ffa30f1f4a99501f97d6c0d29219
do-bby <qkrdbstn25@naver.com>
2022-03-29 16:43:36 +0900

    bean

---
+++ lombok적용.java
@@ -0,0 +1,20 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    //lombok라이브러리가 제공하는 @RequiredArgsConstructor기능사용하면 final이 붙은 필드 생성자를 자동으로 생성해줌
+}
---
+++ 빈 생명주기.java
@@ -0,0 +1,24 @@
+﻿package hello.core.lifecycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
+    
+    @PostConstruct
+    public void init(){
+        connect();
+        call("초기화 연결 메시지");
+    }
+    @PostConstruct를 사용하면 빈 생성하기 전에 해당 메소드 실행
+
+
+    @PreDestroy
+    public void close(){
+        disconnect();
+    }
+    //@PreDestroy를 사용하면 빈 종료하기 전에 해당 메소드 실행
+
+    //위 방법과 다르게 빈 설정 정보에 
+    //@Bean(initMethod = "init", destroyMethod = "close")처럼 초기화, 소멸 메소드 지정 가능
+}
